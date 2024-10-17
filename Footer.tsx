39642ebc3203020501798b50f92c570d41b36c9a
pulcovamon <pulcovamon@gmail.com>
2024-10-17 19:34:20 +0200

    implement footer

---
+++ Footer.tsx
@@ -0,0 +1,42 @@
+// Footer.js
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Footer = ({ navigation }) => {
+  return (
+    <View style={styles.footerContainer}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+        <Icon name="home" size={60} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Camera')}>
+        <Icon name="photo-camera" size={60} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => alert('Galerie bude dostupná brzy!')}>
+        <Icon name="image" size={60} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#6200EE', // Fialová barva
+    paddingVertical: 10,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  button: {
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    marginTop: 5,
+  },
+});
+
+export default Footer;
