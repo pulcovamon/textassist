471c3a300a945130e3241f152af8f44809216825
pulcovamon <pulcovamon@gmail.com>
2024-10-17 23:48:20 +0200

    use atkinson hyperlegible font

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import HomeScreen from "./HomeScreen";
 import CameraScreen from "./CameraScreen";
 import ResultScreen from "./ResultScreen";
+import GalleryScreen from "./GalleryScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    'Atkinson-Hyperlegible': require('./assets/fonts/Atkinson-Hyperlegible.ttf'),
+    'Atkinson-Hyperlegible-bold': require('./assets/fonts/Atkinson-Hyperlegible-bold.ttf'),
+  });
+
+  console.log(fontsLoaded);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -19,6 +32,11 @@ const App = () => {
         <Stack.Screen
           name="Camera"
           component={CameraScreen}
+          options={{ headerShown: false }}
+        />
+       <Stack.Screen
+          name="Gallery"
+          component={GalleryScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
