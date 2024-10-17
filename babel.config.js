bf32f97bf82ef95a7430e7dba6fa767721bcf995
pulcovamon <pulcovamon@gmail.com>
2024-10-17 19:58:13 +0200

    update some configurations

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+      },
+    ],
+    'react-native-reanimated/plugin',
+    [
+      '@babel/plugin-transform-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-transform-private-methods',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      {
+        loose: true,
+      },
+    ],
+  ],
 };
